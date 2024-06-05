48d08f348fbd5eb2beaa5e2e2a8946788c3243d6
Hongjunyeong <105625862+gaenyangi@users.noreply.github.com>
2024-06-05 07:06:29 -0700

    Add files via upload

--- graph.cpp
+++ graph.cpp
@@ -26,7 +26,7 @@ private:
     void heapifyUp(int i);
 };
 
-//constructor of Binary heap
+// constructor of Binary heap
 BinaryHeap::BinaryHeap(int capacity) : size(0), pos(capacity, -1) {}
 
 //insertion function
@@ -47,14 +47,14 @@ void BinaryHeap::decreaseKey(int v, int key) {
 //Extract min function
 int BinaryHeap::extractMin() {
     if (size == 0) return -1;
-    int minVertex = heap[0].second;
-    swap(heap[0], heap[size - 1]);
+    int minVertex = heap[0].second; // root is min
+    swap(heap[0], heap[size - 1]); // swap root with last element
     pos[heap[0].second] = 0;
     pos[minVertex] = -1;
     heap.pop_back();
     size--;
-    heapifyDown(0);
-    return minVertex;
+    heapifyDown(0); // and min_heapify again
+    return minVertex; // return smallest
 }
 
 bool BinaryHeap::isEmpty() const {
@@ -82,14 +82,13 @@ void BinaryHeap::heapifyDown(int i) {
     }
 }
 
-/*
 void BinaryHeap::heapifyUp(int i) {
     while (i && heap[i].first < heap[(i - 1) / 2].first) {
         swap(pos[heap[i].second], pos[heap[(i - 1) / 2].second]);
         swap(heap[i], heap[(i - 1) / 2]);
         i = (i - 1) / 2;
     }
-}*/
+}
 
 /*-----------------------------MSt class implementing prim's algorithm with heap-------------------------*/
 
@@ -101,21 +100,22 @@ public:
 
 private:
     int vertices;
-    vector<vector<pair<int, int>>> adj;
+    vector<vector<pair<int, int>>> adj; //adjacent matrix
 };
 
+//constructor
 MST::MST(int vertices) : vertices(vertices), adj(vertices) {}
 
-//push edge of node u/v and v/w into adj matrix
+//push edge of node u/v with weight w into adj matrix
 void MST::addEdge(int u, int v, int w) {
     adj[u].push_back({v, w});
-    adj[v].push_back({u, w});
+    adj[v].push_back({u, w}); // add both way cuz it's undirected graph
 }
 
 //constructing minimum spanning tree at output.txt with given input
 void MST::primMST(int start, const string& output_file) {
-    vector<int> key(vertices, numeric_limits<int>::max());
-    vector<int> parent(vertices, -1);
+    vector<int> key(vertices, 99999); //default MST init with largest value
+    vector<int> parent(vertices, -1); // default pred tree
     vector<bool> inMST(vertices, false);
     BinaryHeap pq(vertices);
 
@@ -126,13 +126,19 @@ void MST::primMST(int start, const string& output_file) {
         int u = pq.extractMin();
         inMST[u] = true;
 
-        for (auto& [v, weight] : adj[u]) {
+        for (size_t i = 0; i < adj[u].size(); ++i) {
+            int v = adj[u][i].first;
+            int weight = adj[u][i].second;
+
+            //check if shorter path exists and swap
             if (!inMST[v] && weight < key[v]) {
                 key[v] = weight;
                 parent[v] = u;
+                //if exist, decrease it's value
                 if (pq.inHeap(v)) {
                     pq.decreaseKey(v, weight);
                 } else {
+                    //if it's new, add it to heap
                     pq.insert(v, weight);
                 }
             }
@@ -146,6 +152,7 @@ void MST::primMST(int start, const string& output_file) {
     output_file_stream.close();
 }
 
+
 /*--------------------------------- APSP class for Floyd-warshall algorithm--------------------------------*/
 
 class APSP {
@@ -157,25 +164,28 @@ private:
     int vertices;
 };
 
+//constructor
 APSP::APSP(int vertices) : vertices(vertices) {}
 
 void APSP::floydWarshall(const vector<vector<int>>& graph, const string& output_file) {
-    vector<vector<int>> dist = graph;
-    vector<vector<int>> pred(vertices, vector<int>(vertices, -1));
+    vector<vector<int>> dist = graph; // input adj matrix
+    vector<vector<int>> pred(vertices, vector<int>(vertices, -1)); // pred matrix is init as -1
 
+    //init values for pred matrix
     for (int i = 0; i < vertices; ++i) {
         for (int j = 0; j < vertices; ++j) {
-            if (i != j && graph[i][j] != numeric_limits<int>::max()) {
+            if (i != j && graph[i][j] != 99999) {
                 pred[i][j] = i;
             }
         }
     }
 
+    //triply nested for loop of floyd-warshall
     for (int k = 0; k < vertices; ++k) {
         for (int i = 0; i < vertices; ++i) {
             for (int j = 0; j < vertices; ++j) {
-                if (dist[i][k] != numeric_limits<int>::max() && dist[k][j] != numeric_limits<int>::max() &&
-                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                // check whether dist relaxated through k is shorter, and update
+                if (dist[i][k] != 99999 && dist[k][j] != 99999 && dist[i][k] + dist[k][j] < dist[i][j]) {
                     dist[i][j] = dist[i][k] + dist[k][j];
                     pred[i][j] = pred[k][j];
                 }
@@ -183,13 +193,16 @@ void APSP::floydWarshall(const vector<vector<int>>& graph, const string& output_
         }
     }
 
+    //print the result to output_sp.txt
     ofstream output_file_stream(output_file);
 
+    //shortest path weight matrix D
     output_file_stream << "D " << vertices << "\n";
-    for (const auto& row : dist) {
-        for (int val : row) {
-            if (val == numeric_limits<int>::max()) {
-                output_file_stream << "INF ";
+    for (int i = 0; i < vertices; ++i) {
+        for (int j = 0; j < vertices; ++j) {
+            int val = dist[i][j];
+            if (val == 99999) {
+                output_file_stream << "-1 "; //INF
             } else {
                 output_file_stream << val << " ";
             }
@@ -197,13 +210,16 @@ void APSP::floydWarshall(const vector<vector<int>>& graph, const string& output_
         output_file_stream << "\n";
     }
 
+    // predecessor matrix P
     output_file_stream << "P " << vertices << "\n";
-    for (const auto& row : pred) {
-        for (int val : row) {
-            output_file_stream << (val == -1 ? to_string(-1) : to_string(val+1)) << " ";
+    for (int i = 0; i < vertices; ++i) {
+        for (int j = 0; j < vertices; ++j) {
+            int val = pred[i][j];
+            output_file_stream << (val == -1 ? to_string(-1) : to_string(val + 1)) << " "; //-1 for NIL
         }
         output_file_stream << "\n";
     }
+
 
     output_file_stream.close();
 }
@@ -218,12 +234,13 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    //parse the input arguments
     string input_file_sp = argv[1];
     string input_file_mst = argv[2];
     string output_file_sp = argv[3];
     string output_file_mst = argv[4];
 
-    // read MST input file
+    // open input_mst.txt file
     ifstream mst_input_file(input_file_mst);
     if (!mst_input_file.is_open()) {
         cerr << "Error opening input file: " << input_file_mst << endl;
@@ -246,16 +263,18 @@ int main(int argc, char* argv[]) {
     // call mst class function with input and write the result into output file
     mst.primMST(start_vertex, output_file_mst);
 
-    // read APSP input file
+    // open input_sp.txt
     ifstream sp_input_file(input_file_sp);
     if (!sp_input_file.is_open()) {
         cerr << "Error opening input file: " << input_file_sp << endl;
         return 1;
     }
 
+    //get the number of lines
     int n;
     sp_input_file >> n;
 
+    // get the data of adj matrix
     vector<vector<int>> graph(n, vector<int>(n));
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
